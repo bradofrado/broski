a113a191adcc9838e5d69c30bd50d8d22dcd2313
Braydon Jones <bradofrado@gmail.com>
2022-11-12 16:14:59 -0700

    fixed images

--- client/src/components/ItemListing/ItemListing.js
+++ client/src/components/ItemListing/ItemListing.js
@@ -4,7 +4,7 @@ import Card from "react-bootstrap/Card";
 export default function ItemListing(props) {
   return (
     <Card style={{ width: "18rem" }} text="black">
-      <Card.Img variant="top" src="skis.jpg" />
+      <Card.Img variant="top" src={props.listing.image} />
       <Card.Body>
         <Card.Title>{props.listing.name}</Card.Title>
         <Card.Text>From: {props.listing.seller.firstname}</Card.Text>
