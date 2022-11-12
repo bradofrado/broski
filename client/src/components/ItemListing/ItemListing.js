60e827f9af21eb11ed3753aade275ddd5bd1c0ce
Braydon Jones <bradofrado@gmail.com>
2022-11-12 12:08:26 -0700

    Added dumby data

--- client/src/components/ItemListing/ItemListing.js
+++ client/src/components/ItemListing/ItemListing.js
@@ -1,3 +1,3 @@
 export default function ItemListing(props) {
-    return <>Hello World</>
+    return <>{props.listing.name}</>
 }
--- client/src/views/Home/Home.js
+++ client/src/views/Home/Home.js
@@ -3,13 +3,16 @@ import ImageButton from '../../components/ImageButton/ImageButton';
 import ItemListing from '../../components/ItemListing/ItemListing';
 
 export default function Home() {
+    
+    const listings = [{image: "", price: 5, name: "Snowboard"}];
     return <>
             <h1 className="mb-2">Welcome to our website</h1>
             <div className="d-flex flex-rowcol">
                 {/* <ImageButton className="m-20" to="/home" img="/logo192.png"/>
                 <ImageButton className="m-20" to="/home" img="/logo192.png"/>
                 <ImageButton className="m-20" to="/home" img="/logo192.png"/> */}
-                <ItemListing/>
+                {listings.map(x => <ItemListing listing={x}/>)}
+
             </div>
         </>
 }
